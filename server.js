5fc9ae6067bdb2d2433b013ae39e664bd2eec6d0
qeswelan <otaniyici@gmail.com>
2022-07-06 16:12:26 -0400

    basic chat setup

--- server.js
+++ server.js
@@ -2,14 +2,35 @@ const path = require("path");
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(morgan("dev"));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Run when client connect
+io.on("connection", (socket) => {
+  // Welcome current user
+  socket.emit("message", "Welcome to ChatCord!");
+
+  // Broadcast when a user connects
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
+});
 
 const PORT = 3000 || process.env.PORT;
 
